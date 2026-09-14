5926da72001271330740cd1917024aa1c84a4b12
Patrick Fawcett <patrickfawcett@hotmail.com>
2026-09-14 18:47:54 +1200

    First implemtation of grid snapping behaviour for states and transitions.
    Supports toggling of behaviour and setting arbitrary grid sizes >=1.
    
    Currently does not have a way to modify grid settings in app due to lack of settings window.
    
    Grid settings are also not saved on application shutdown.

---
+++ src/tuataraTMSim/Settings.java
@@ -0,0 +1,63 @@
+package tuataraTMSim;
+
+/**
+ * Holds values and functions for user configurable settings.
+ */
+public final class Settings {
+
+    // Should never instantiate.
+    private Settings() {}
+
+    /**
+     * Dictates snapping behaviour for states and transition lines.
+     */
+    private static boolean m_snapToGrid = true;
+
+    /**
+     * Set grid snapping behaviour.
+     * @param value The new behaviour.
+     */
+    public static void setSnapToGrid(boolean value) {
+        m_snapToGrid = value;
+    }
+
+    /**
+     * Get the current grid snapping behaviour.
+     * @return The current grid snapping behaviour.
+     */
+    public static boolean getSnapToGrid() {
+        return m_snapToGrid;
+    }
+
+    /**
+     * The size of the grid to snap to.
+     */
+    private static int m_gridSize = 5;
+
+    /**
+     * Set the grid size for snapping if enabled.
+     * @param size The new grid size.
+     * @implNote If given size is < 1, it is set to 1.
+     */
+    public static void setGridSize(int size)
+    {
+        m_gridSize = Math.min(1, size);
+    }
+
+    /**
+     * Get the current grid size for snapping if enabled.
+     * @return The current grid size.
+     */
+    public static int getGridSize() {
+        return m_gridSize;
+    }
+
+    /**
+     * Snap a given value to the grid.
+     * @param value The value to snap.
+     * @return The snapped value.
+     */
+    public static int snapValueToGrid(int value) {
+        return (int)Math.round((double)value / m_gridSize) * m_gridSize;
+    }
+}
