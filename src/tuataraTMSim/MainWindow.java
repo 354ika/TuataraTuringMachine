369ae942cc9e1811604272c9e1f0e795f5e6e780
Patrick Fawcett <patrickfawcett@hotmail.com>
2026-09-14 20:07:08 +1200

    Fixed possible Java version dependent error with use of var, swapped to Object instead.

--- src/tuataraTMSim/MainWindow.java
+++ src/tuataraTMSim/MainWindow.java
@@ -3005,7 +3005,7 @@ public class MainWindow extends JFrame
         public void actionPerformed(ActionEvent e)
         {
             Settings.setSnapToGrid(!Settings.getSnapToGrid());
-            var source = e.getSource();
+            Object source = e.getSource();
             if (source.getClass() == JCheckBoxMenuItem.class) {
                 ((JCheckBoxMenuItem) source).setState(Settings.getSnapToGrid());
             }
